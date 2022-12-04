63936bf9a76d1f5ec2f802c7359a612f5e63fb12
Gabriel Martins <87931081+gdlmartins@users.noreply.github.com>
2022-12-05 00:37:37 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("teste 1")
+import express , {Request , Response} from "express";
+
+
+const server = express();
+
+server.use(express.json())
+
+
+server.get("/",(req : Request , res: Response)=>{
+ return res.status(200).json({
+     message: "DioBank API"
+ })
+} )
+
+
+server.post("/user" ,(req :Request , res :Response)=>{
+   const body = req.body;
+   console.log(body)
+   return res.status(201).json({message: "user created"})
+
+})
+
+server.listen(3003, ()=> console.log("Server is running"))
